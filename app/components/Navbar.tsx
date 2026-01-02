c82642da6cfec6427b18559a01e783369a4edf3e
chollada punseenam <chollada.pu@ku.th>
2026-01-02 17:36:51 +0700

    Refactor Navbar button styles for improved responsiveness and consistency

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar()
     return(
         <div className="w-screen flex flex-row justify-between align-middle items-center pi:mx-3 2xl:mx-5">
             <h1 className="font-black pi:text-[32px] 2xl:text-[40px]  text-[#4F46E5] ">Secure Remote Signature</h1>
-            <button className="font-bold text-[#ffffff] p-3 px-5 bg-indigo rounded-[30px] hover:opacity-70 text-[24px]">สร้างบัญชีใหม่</button>
+            <button className="font-bold text-[#ffffff] pi:my-2 pi:p-2 pi:px-3 2xl:p-3 2xl:px-5 bg-indigo rounded-[30px] hover:opacity-70 pi:text-[18px] 2xl:text-[20px]">สร้างบัญชีใหม่</button>
         </div>
     );
 }
